03c767920a03fa39e6732308caec2f5854c1c7d1
Trentin1 <thiagotrentini@gmail.com>
2026-03-14 14:20:34 -0300

    Segundo exercicio pronto

--- exercicioforwhile.py
+++ exercicioforwhile.py
@@ -5,8 +5,16 @@ for i in range(1, 11):
     print(f"Line: {i}")
 
 # Exercicio 2
-# Crie um programa que comece com o número 1 e vá aumentande de 1 em 1 até chegar em 5,
+# Crie um programa que comece com o número 1 e vá aumentando de 1 em 1 até chegar em 5,
 # mostrando cada número na tela.
+
+number = 5
+x = 1
+
+while x < number:
+    x += 1
+
+print(x)
 
 # Exercicio 3
 # Escreva dois programas: 1. Um com for que soma os números de 1 até 5.
