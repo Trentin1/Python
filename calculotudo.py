4b7e7c2dc95235456ceebe5674482f24b28d6ea8
Trentin1 <thiagotrentini@gmail.com>
2026-03-13 22:08:31 -0300

    Adicionando soma.py

---
+++ calculotudo.py
@@ -0,0 +1,23 @@
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+c = int(input("Digite o valor de c: "))
+
+# Calculo de soma
+soma = a + b
+print("Soma de a + b: ", soma)
+
+# Calculo de subtração
+sub = a + b - c
+print("Está é o resultado de a + b - c, é: ", sub)
+
+# Calculo de multiplicação
+mul = a * c
+print("A multiplicação de a * c, foi: ", mul)
+
+# Calculo de divisão
+div = b / c
+print("A divisão de b / c, é:", div)
+
+# Calculo de tudo
+tudo = a + b + c - b * c / 3
+print("A média é : ", tudo)
---
+++ soma2.py
@@ -0,0 +1,17 @@
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+c = int(input("Digite o valor de c:"))
+
+# Calculo de soma
+soma = a + b
+print("Soma de a + b: ", soma)
+
+# Calculo de subtração
+sub = a + b - c
+print("Está é o resultado de a + b - c: ", sub)
+
+# Calculo de multiplicação
+
+# Calculo de divisão
+
+# Calculo de tudo
