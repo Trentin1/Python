fe45eee4c6bf01d03b440ce434bf9649655d2d44
Trentin1 <thiagotrentini@gmail.com>
2026-03-15 22:41:44 -0300

    Desafio de maestria Booleano

---
+++ desafiobool.py
@@ -0,0 +1,9 @@
+# Você recebeu um código. Atribua valores a x1 e x2 de forma que x3 resulte em False.
+
+# x1 e x2 devem ser atribuídos valores booleanos (True ou False) ou expressões booleanas.
+
+x1 = False
+x2 = False
+
+x3 = x1 and x2
+print(f"x3 = {x3}")
