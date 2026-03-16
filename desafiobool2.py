85b2a4cf6a2b262297b242afc961b16c47c8c86d
Trentin1 <thiagotrentini@gmail.com>
2026-03-15 22:47:37 -0300

    Desafio de Maestria Bool2

---
+++ desafiobool2.py
@@ -0,0 +1,12 @@
+# Substitua os valores das variáveis a, b e c com valores booleanos (True ou False)
+# para que result seja avaliado como True.
+
+# result será True quando a expressão (a or b) and not c for verdadeira.
+# Use os operadores lógicos or, and e not para resolver este desafio.
+
+a = True
+b = True
+c = False
+
+result = (a or b) and not c
+print(f"result = {result}")
