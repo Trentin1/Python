3d6c839ec797f829b80ac39443bd15522869586e
Trentin1 <thiagotrentini@gmail.com>
2026-03-16 22:31:54 -0300

    Organizei os arquivos .py na pasta Python

---
+++ Python/abaoutloopwhile.py
@@ -0,0 +1,10 @@
+# while condition:
+#    code
+
+number = 27
+power_of_two = 1
+
+while power_of_two <= number:
+    power_of_two *= 2
+
+print(power_of_two)
---
+++ Python/desafioif.py
@@ -0,0 +1,17 @@
+# Desafio Iniciante
+# As variáveis a e b têm valores ausentes. Preencha-as para que o código dentro da declaração if seja executado! (certifique-se de que a condição do if seja verdadeira)
+
+# No final do programa, o valor de c deve ser 3.
+
+# Bônus: tente encontrar mais de uma solução!
+
+# Variavéis
+a = 7
+b = 13
+
+c = 0
+if a < b or b >= 10:
+    c = 2
+
+c += 1
+print(f"c = {c}")
