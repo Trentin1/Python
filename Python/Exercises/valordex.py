eac2a1d3df2ad6f671f412921dfd7aaa6ff369eb
Trentin1 <thiagotrentini@gmail.com>
2026-03-22 23:35:05 -0300

    Coddy.Tech

---
+++ Python/Exercises/valordex.py
@@ -0,0 +1,11 @@
+x = 10
+print("Alto" if x > 5 else "Baixo")
+
+a = 20
+b = 10
+
+res = a if a > b else b
+print("o valor é", res)
+
+y = int(input("Digite sua idade: "))
+print("Maior de idade" if y >= 18 else "Menor de idade")
