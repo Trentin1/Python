53703c48e2b9268a144f53f43b1740f13f668f9f
Trentin1 <thiagotrentini@gmail.com>
2026-03-25 15:15:56 -0300

    Challenge Arguments - Coddy.Tech - Att

--- Python/Exercises/challengeArguments.py
+++ Python/Exercises/challengeArguments.py
@@ -1,12 +1,12 @@
 # Declare yout function here
 def your_function_name(parameter1, parameter2):
     # calculate product and print it
-    print(parameter1 * parameter2)
+    return parameter1 * parameter2
 
 
 a = int(input())
 b = int(input())
 # call your function here with the arguments a and b
 
-your_function_name(a, b)
-print(your_function_name)
+resultado = your_function_name(a, b)
+print(resultado)
